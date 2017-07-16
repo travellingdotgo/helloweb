d46b8ce50665a73199ae2d59f3d410a394d29727
c <c@g.hk>
2017-07-16 09:27:15 +0800

    update

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -13,17 +13,17 @@ public class HelloWorld extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        message = "Hello world, this message is from servlet!";
+        message = "init: "+System.currentTimeMillis();//"Hello world, this message is from servlet!";
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //设置响应内容类型
-        resp.setContentType("text/html");
+        resp.setContentType("json");
 
         //设置逻辑实现
         PrintWriter out = resp.getWriter();
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "\n\nget: " + System.currentTimeMillis() + "</h1>");
     }
 
     @Override
